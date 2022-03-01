3c87d671d16947bd03709d90feed4f2679dc70b4
Rommanirs Mer <rommanirs.mer@outlook.fr>
2022-03-01 12:57:22 +0100

    Fix Button interaction due to multiple interactable object

---
+++ Assets/Scripts/ButtonTest.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonTest : MonoBehaviour
+{
+    public void test()
+    {
+        Debug.Log("Button is working");
+    }
+}
